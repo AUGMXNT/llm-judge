26009b95685329dae9b785a2dd336b0d1cd3ee82
lhl <lhl@randomfoo.net>
2023-12-21 21:38:59 +0900

    AWQ support

--- gen_model_answer.py
+++ gen_model_answer.py
@@ -95,7 +95,10 @@ def get_model_answers(
 	        tokenizer.chat_template = "{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
 
     # vLLM
-    llm = LLM(model=model_path, tensor_parallel_size=num_gpus_per_model)
+    if model_path.find("AWQ") >= 0:
+        llm = LLM(model=model_path, tensor_parallel_size=num_gpus_per_model, quantization="AWQ")
+    else:
+        llm = LLM(model=model_path, tensor_parallel_size=num_gpus_per_model)
 
     for question in tqdm(questions):
         if question["category"] in temperature_config:
